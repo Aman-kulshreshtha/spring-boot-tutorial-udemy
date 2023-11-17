8e0c760b71b82e2d021de29e0285f59350255b49
amankulshreshtha <aman21102000@gmail.com>
2023-11-17 19:49:37 +0530

    added demo code for showing concept of injecting custom properties into code

--- src/main/java/com/example/demo/rest/controller/FunRestController.java
+++ src/main/java/com/example/demo/rest/controller/FunRestController.java
@@ -1,10 +1,19 @@
 package com.example.demo.rest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // injecting custom properties from properties file namely machine.name and machine.company
+
+    @Value("${machine.name}")
+    private String machineName;
+
+    @Value("${machine.company}")
+    private String machineCompany;
 
 
     // exposes a simple "/" endpoint that will return a string "Hello World!" at the endpoint
@@ -17,6 +26,11 @@ public class FunRestController {
     @GetMapping("/work")
     public String getWork() {
         return "Your work for today is to chill !!";
+    }
+
+    @GetMapping("/machine")
+    public String getMachineDetails() {
+        return "This is running on an "+ machineCompany + " " + machineName;
     }
 
 }
