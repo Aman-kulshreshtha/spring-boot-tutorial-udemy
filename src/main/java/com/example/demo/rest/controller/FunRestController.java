eeeefd7a77e9ee3607b633ed5809b5928e8d86f9
amankulshreshtha <aman21102000@gmail.com>
2023-11-17 16:11:32 +0530

    used properties from application.properties file

--- src/main/java/com/example/demo/rest/controller/FunRestController.java
+++ src/main/java/com/example/demo/rest/controller/FunRestController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +9,7 @@ public class FunRestController {
 
     // exposes a simple "/" endpoint that will return a string "Hello World!" at the endpoint
     // for local the endpoint is "http://localhost:8080/"
-    @GetMapping("/")
+    @GetMapping("${url.greet}") // using greet url from the properties file ( application.properties ) in this case /greet
     public String sayHello() {
         return "Hello World!";
     }
