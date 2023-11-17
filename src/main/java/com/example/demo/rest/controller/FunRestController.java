3f18aee1d8304c124b6ba99fa05a6c783677d820
amankulshreshtha <aman21102000@gmail.com>
2023-11-17 17:24:53 +0530

    added spring boot actuator

--- src/main/java/com/example/demo/rest/controller/FunRestController.java
+++ src/main/java/com/example/demo/rest/controller/FunRestController.java
@@ -9,7 +9,7 @@ public class FunRestController {
 
     // exposes a simple "/" endpoint that will return a string "Hello World!" at the endpoint
     // for local the endpoint is "http://localhost:8080/"
-    @GetMapping("${url.greet}") // using greet url from the properties file ( application.properties ) in this case /greet
+    @GetMapping("/") // using greet url from the properties file ( application.properties ) in this case /greet
     public String sayHello() {
         return "Hello World!";
     }
