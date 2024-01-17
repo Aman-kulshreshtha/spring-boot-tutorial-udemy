2885dcdecf6ef360c724c83c2d3f3a839b37789c
amankulshreshtha <aman21102000@gmail.com>
2024-01-18 00:39:15 +0530

    code for access based on roles

--- src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -2,9 +2,13 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -33,4 +37,30 @@ public class SecurityConfiguration {
         return new InMemoryUserDetailsManager(john,mary,susan);
 
     }
+
+    // COde for restricting access based on roles
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeHttpRequests(
+                config-> {
+                    config.requestMatchers(HttpMethod.GET,"/api/employee").hasRole("EMPLOYEE")
+                            .requestMatchers(HttpMethod.GET,"/api/employee/**").hasRole("EMPLOYEE")
+                            .requestMatchers(HttpMethod.PUT,"/api/employee/**").hasRole("MANAGER")
+                            .requestMatchers(HttpMethod.POST,"/api/employee").hasRole("MANAGER")
+                            .requestMatchers(HttpMethod.DELETE,"/api/employee/**").hasRole("ADMIN");
+                }
+        );
+
+        // tell spring boot to use basic auth
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        //disable csrf
+        // in general csrf is not required for stateless REST api calls that use methods like POST, PUT, PATCH or DELETE
+        httpSecurity.csrf(csrf->csrf.disable());
+
+        return httpSecurity.build();
+    }
+
 }
