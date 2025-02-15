8aad78a08a4565be0caed42f4f09ffef1c322e17
amankulshreshtha <aman21102000@gmail.com>
2025-02-16 00:03:26 +0530

    di code

--- src/main/java/com/example/demo/config/SportsCoachConfig.java
+++ src/main/java/com/example/demo/config/SportsCoachConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import com.example.demo.coaches.BaseballCoach;
 import com.example.demo.coaches.CricketCoach;
 import com.example.demo.interfaces.Coach;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class SportsCoachConfig {
 
     @Bean
+    @Qualifier("cricket")
     public Coach cricketCoach() {
         return new CricketCoach();
     }
